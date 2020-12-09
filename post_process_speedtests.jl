45b328f2927425cb7c87a3aaddffbf6fa2d15ddc
isaacdevlugt <ijsdevlugt@uwaterloo.ca>
2020-12-09 16:55:28 -0500

    Time series

--- post_process_speedtests.jl
+++ post_process_speedtests.jl
@@ -4,28 +4,26 @@ function get_download(fname)
 
     words = readlines(fname)
 
-    downloads = Any[]
-    uploads = Any[]
-    pings = Any[]
+    downloads = []
+    uploads = []
+    pings = []
 
     for (i, word) in enumerate(words)
-        
-        if startswith(word, "Download:")
-            tmp = split(word, r" ")
-            push!(downloads, parse(Float64, tmp[length(tmp)-1]))
-        end
-        
-        if startswith(word, "Upload:")
-            tmp = split(word, r" ")
-            push!(uploads, parse(Float64, tmp[length(tmp)-1]))
-        end
-        
-        if startswith(word, "Selecting best server based on ping...")
+       
+        if startswith(word, "Testing download")
             tmp = split(words[i+1], r" ")
+            push!(downloads, parse(Float64, tmp[length(tmp)-1]))
+        
+        elseif startswith(word, "Testing upload")
+            tmp = split(words[i+1], r" ")
+            push!(uploads, parse(Float64, tmp[length(tmp)-1]))
+        
+        elseif startswith(word, "Hosted by")
+            tmp = split(word, r" ")
             push!(pings, parse(Float64, tmp[length(tmp)-1]))
+       
         end
-
-
+        
     end
     return downloads, uploads, pings
 
@@ -33,16 +31,16 @@ end
 
 down, up, ping = get_download("speedtests")
 
-deleteat!(up, length(up))
-deleteat!(ping, length(ping))
+x = range(0, stop=length(down)-1)
+time = readlines("time_series")
 
-time = range(1, stop=length(down))
-ioff()
 fig, ax = subplots(3,1, sharex = true)
 ax[1,1].plot(time, down, color="blue")
-ax[1,1].set_ylabel("Down (Mbit/s)")
+ax[1,1].set_ylabel("Down (Mbs)")
 ax[2,1].plot(time, up, color="red")
-ax[2,1].set_ylabel("Up (Mbit/s)")
+ax[2,1].set_ylabel("Up (Mbs)")
 ax[3,1].plot(time, ping, color="green")
 ax[3,1].set_ylabel("Ping (ms)")
-savefig("speedtest.png")
+plt.xticks(x, time, rotation="vertical")
+plt.tight_layout()
+savefig("speedtest.png", format="png", dpi=1000)
--- run_speedtest.sh
+++ run_speedtest.sh
@@ -1,1 +1,1 @@
-for i in {1..64} ; do echo -n "Speed test $i "; speedtest >> speedtests ; sleep 900 ; done
+for i in {1..432} ; do echo -n "Speed test $i "; date +"%FT%T" >> time_series ; speedtest >> speedtests ; sleep 300 ; done
